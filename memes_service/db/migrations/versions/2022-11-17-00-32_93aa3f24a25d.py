45c8488267700f89c4b63d6787fa8b2916df3f27
aleh <melnik.aleh.94@gmail.com>
2022-11-26 16:25:25 +0300

    init

---
+++ memes_service/db/migrations/versions/2022-11-17-00-32_93aa3f24a25d.py
@@ -0,0 +1,21 @@
+"""empty message
+
+Revision ID: 93aa3f24a25d
+Revises:
+Create Date: 2022-11-17 00:32:58.197014
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "93aa3f24a25d"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
